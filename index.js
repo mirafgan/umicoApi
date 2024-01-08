b3d84bf5c9e86ef73c70c24e255f94c4110147cc
mirafgan <mirefqan1999@gmail.com>
2024-01-09 01:38:11 +0400

    vercel.json

--- index.js
+++ index.js
@@ -14,5 +14,17 @@ app.use(
     swaggerUi.setup(specs)
 );
 app.get("/",(req,res)=>res.send("salam"))
+app.get('/categories', async (req, res) => {
+    const { data } = await fetchData("https://umico.az/catalog/v3/market/categories");
+    const filter = ['id', "name", "slugged_name", "icons", "child_ids", "ascii_icon"]
+    const keys = Object.keys(data[0]);
+    const changeData = data.map((item) => {
+        keys.forEach((i) => {
+            if (!filter.includes(i)) item[i] = undefined
+        })
+        return item
+    })
+    res.send(changeData)
+})
 routes(app)
 app.listen(5000)

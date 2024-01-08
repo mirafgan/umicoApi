d0f08387ed607c0cadd5a0da2fb2219562795565
mirafgan <mirefqan1999@gmail.com>
2024-01-09 01:30:36 +0400

    vercel.json

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const app = express();
 const cors = require("cors")
 const bodyParser = require("body-parser")
 app.use(cors())
+app.use(bodyParser.json())
 app.use(
     "/api-docs",
     swaggerUi.serve,
     swaggerUi.setup(specs)
 );
-app.use(bodyParser.json())
 
 routes(app)
 app.listen(3001)
